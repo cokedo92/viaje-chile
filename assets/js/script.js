baa7970bb25d586fbd97fbdc5599a7227e3c2ce5
Jorge Donaire <cokedo92@gmail.com>
2024-02-05 01:34:10 -0300

    Se creo la funcion en el script para desaparecer las tarjetas con un klik

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,14 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
     tooltipTriggerList.forEach(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
-// color rojo en franja de quienes somos
+// color franja de quienes somos
 $(".franja").on("dblclick", function () {
     $(this).css("background-color", "red");
 });
 
+// Desaparicion de las tarjetas
+$(".destacadosTitulo").on("click", function () {
+    $(".card").toggle();
+});
 
 
